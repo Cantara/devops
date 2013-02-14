482b81029cb55a89bb9ed38a32113462e6164081
Erik Drolshammer <erik.drolshammer@tomra.com>
2013-02-14 09:06:16 +0100

    Create a folder structure and add some comments to deployFromNexus script.

--- pull_deploy/win/deployFromNexus.bat
+++ pull_deploy/win/deployFromNexus.bat
@@ -9,8 +9,8 @@ rem
 rem Usge: DeployFromNexus repogroup artifact version
 rem
  
-echo Verify parameters
-call :VERIFYPARAMETERS
+rem echo Verify parameters
+rem call :VERIFYPARAMETERS
 echo Set variables
 call :SETVARIABLES
 echo Download artifact
@@ -23,17 +23,34 @@ call BackupAndInstall.bat %ARTIFACT% %VERSION% %ARTIFACT%-%VERSION%
 goto :END
  
 :SETVARIABLES
+rem 
+rem
+rem
+rem To get a proper file name when wget follows redirects
+rem wget --content-disposition --no-check-certificate --trust-server-names=on "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=no.yenka.giftit&a=giftit-deploy-mobile&v=LATEST&c=android&p=apk"
+rem
+rem
+rem Find latest file on the file system: http://stackoverflow.com/questions/97371/how-do-i-write-a-windows-batch-script-to-copy-the-newest-file-from-a-directory
+rem
+rem 
+rem TODO Add support for username and password when downloading
+rem
+rem
 rem Unclear to test on my repo which is empty. set BASE_URL=http://127.0.0.1/nexus/service/local/artifact/maven/redirect?
-set BASE_URL=http://repository.sonatype.org/service/local/artifact/maven/redirect?
-set RELEASE_REPO=central-proxy
-set SNAPSHOT_REPO=
-set GROUP=log4j
-set ARTIFACT=log4j
+rem set BASE_URL=http://repository.sonatype.org/service/local/artifact/maven/redirect?
+set BASE_URL=http://mvnrepo.cantara.no/service/local/artifact/maven/redirect?
+rem set RELEASE_REPO=central-proxy
+rem set SNAPSHOT_REPO=
+set REPO=snapshots
+set GROUP=no.yenka.giftit
+set ARTIFACT=giftit-deploy-mobile
 set VERSION=LATEST
+set CLASSIFIER=android
+set PACKAGING=apk
 EXIT /B
  
 :DOWNLOAD
-set URL="%BASE_URL%r=%RELEASE_REPO%&g=%GROUP%&a=%ARTIFACT%&v=%VERSION%"
+set URL="%BASE_URL%r=%REPO%&g=%GROUP%&a=%ARTIFACT%&v=%VERSION%&c=%CLASSIFIER%&p=%PACKAGING%"
 set FILENAME=%ARTIFACT%-%VERSION%.zip
 rem TODO improvement - filename should actually reflect the version downloaded
 rem Setting static filename to be able to work on it after.
