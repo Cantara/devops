fc784c0c938a5213f9b8a09c61404e09b5cf2284
Anders Emil Rønning <andeero@ifi.uio.no>
2015-11-20 13:10:47 +0100

    Revamped Docker build configuration

--- pull_deploy/linux/build/Docker/test-docker.sh
+++ pull_deploy/linux/build/Docker/test-docker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker build -t $BASE_IMAGE-dev .
+docker build -t configservice-dev .
 
 CONFIG_FILE="$(dirname $(pwd))/config_override.properties"
-VERSION=SNAPSHOT
 
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "config_override.properties in parent directory is missing"
@@ -14,12 +13,11 @@ LOCAL=""
 if [ "$1" == "local" ]; then
     (cd .. && mvn package) || exit 2
     JAR=$(cd ../target && ls -1t *.jar | head -1)
-    LOCAL="-v $(dirname $(pwd))/target/$JAR:/home/$BASE_IMAGE/example-app.jar"
+    LOCAL="-v $(dirname $(pwd))/target/$JAR:/home/configservice/configservice.jar"
 fi
 
-echo "Starting instance. Do 'docker exec -it $BASE_IMAGE-dev bash' to get shell"
+echo "Starting instance. Do 'docker exec -it configservice-dev bash' to get shell"
 
-CONFIG="-v $CONFIG_FILE:/home/$BASE_IMAGE/config_override.properties"
-
+CONFIG="-v $CONFIG_FILE:/home/configservice/config_override.properties"
 docker run --rm -p 8086:8086 --name configservice-dev $CONFIG $LOCAL configservice-dev
 
