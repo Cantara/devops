a7f7f64904791e686430f54d1ebd1aa3aacd3253
Erik Drolshammer <erik-dev@fjas.no>
2015-11-26 14:18:48 +0100

    remove APP variable as this must always be overridden + change path logback.xml and smaller Xms and Xmx defaults

--- pull_deploy/linux/build/Docker/application_scripts/start-service.sh
+++ pull_deploy/linux/build/Docker/application_scripts/start-service.sh
@@ -4,8 +4,7 @@
 
 date +" --- RUNNING $(basename $0) %Y-%m-%d_%H:%M:%S --- "
 
-APP=configservice.jar
-JAVA_PARAMS="-Dlogback.configurationFile=./logback-default.xml -Xms128m -Xmx1024m"
+JAVA_PARAMS="-Dlogback.configurationFile=./config_override/logback.xml -Xms64m -Xmx256m"
 source config_override/service_override.properties # this might override JAVA_PARAMS and version to run
 
 START_APP_COMMAND="/usr/bin/java $JAVA_PARAMS -jar $APP"
