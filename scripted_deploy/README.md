55afe476c67a32f075ab8df95fa343f304f557fb
baardl <bard.lind@gmail.com>
2017-04-05 12:54:31 +0200

    Create Service!

---
+++ scripted_deploy/README.md
@@ -0,0 +1,9 @@
+# Create a new Service     
+* Create user
+* Create su_to scripts
+* Create update service script
+* Create start service script
+
+```
+totod
+```
--- scripted_deploy/create-service.sh
+++ scripted_deploy/create-service.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] ; then
-  echo "Usage: create-service.sh <ServiceName>"
+  echo "Usage: create-service.sh <ServiceName> <com.group.id>"
   exit 1
 fi
 servicename=$1
 username=$1
+if [ -z "$2" ]; then
+  //empty
+  groupid=TODO-set-groupid
+else
+  #Replace / with \/
+  groupid="$(echo $2 |sed 's,\.,\\\/,g')"
+  #groupid=$2
+fi
+
 echo Create User $username
 if id "$username" >/dev/null 2>&1; then
   echo "User exists, not updating."
@@ -21,19 +30,43 @@ if [ -e $suToFile ]; then
 else
   echo '#!/bin/bash' > $suToFile
   echo 'sudo su - '$username >> $suToFile
+  chmod +x $suToFile
 fi
 
-echo create start-service.sh
+echo Create start-service.sh
 startServiceFile=/home/$username/start-service.sh
 if [ -e $startServiceFile ]; then
   echo "File $startServiceFile already exists!"
 else
   echo '#!/bin/bash' | sudo tee --append $startServiceFile
   echo 'nohup /usr/bin/java  -jar /home/'$username'/'$servicename'.jar' | sudo tee --append $startServiceFile
+  sudo chmod +x $startServiceFile
+fi
+
+echo Create update-service.sh
+updateServiceFile=/home/$username/update-service.sh
+if [ -e $updateServiceFile ]; then
+  echo "File $updateServiceFile already exists!"
+else
+  curl -o tmp-update-service.sh https://raw.githubusercontent.com/Cantara/devops/master/scripted_deploy/update-service-template.sh
+  sed -i 's/{artifact-id}/'$servicename'/g' tmp-update-service.sh
+  sed -i 's/{group-id}/'$groupid'/g' tmp-update-service.sh
+  sudo mv tmp-update-service.sh $updateServiceFile
+  sudo chmod +x $updateServiceFile
 fi
 
 echo Set Ownership to files
 sudo chown $username:$username $startServiceFile
+sudo chown $username:$username $updateServiceFile
+
+echo *****************************************************
+echo ** Next steps:
+echo ** 1. ./$suToFile
+echo ** 2. ./update-service.sh
+echo ** 3. ./start-service.sh
+echo **
+echo ** Well Done!
+echo *****************************************************
 
 
 
