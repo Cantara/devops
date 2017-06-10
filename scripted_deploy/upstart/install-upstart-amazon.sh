763f665745bb6c3b3c0dc643250233dc3c03af43
baardl <bard.lind@gmail.com>
2017-06-10 11:25:22 +0200

    ServiceName now on input parameter

--- scripted_deploy/upstart/install-upstart-amazon.sh
+++ scripted_deploy/upstart/install-upstart-amazon.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
- servicename=shareprocApi
- javabin=/usr/bin/java
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+  echo "Usage: install-upstart-amazon.sh <ServiceName> "
+  exit 1
+fi
 
+servicename=$1
+javabin=/usr/bin/java
 
+curl -o upstart-template-amazon.conf https://raw.githubusercontent.com/Cantara/devops/master/scripted_deploy/upstart/upstart-template-amazon.conf
 cp upstart-template-amazon.conf upstart-amazon.conf
 sed -i 's/{servicename}/'$servicename'/g' upstart-amazon.conf
 #sed -i 's/{javabin}/'$javabin'/g' upstart-amazon.conf
 sudo cp upstart-amazon.conf /etc/init/$servicename.conf
+
+echo Script is saved to /etc/init/$servicename.conf
+echo To start this service type: 
+echo 
+echo      "sudo initctl start $servicename"
+echo
