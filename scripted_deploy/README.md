c34b6143704c0058c144a94117cea1ca5e08986a
Anders Rønning <andeero@ifi.uio.no>
2018-08-31 15:45:09 +0200

    Support for semantic versioning in upgrade-service script
    - Renamed script as it can be used as is
    - Includes a simple test script for verifying artifact resolving based on pattern functionality

--- scripted_deploy/README.md
+++ scripted_deploy/README.md
@@ -14,7 +14,7 @@ curl -o create-service.sh https://raw.githubusercontent.com/Cantara/devops/maste
 ### Download jar, and start service
 ```
 ./su_to_<ServiceName>
-./update-service.sh
+./update-service.sh # see --help for arguments
 ./start-service.sh
 ```
 # Install as upstart/initctl
---
+++ scripted_deploy/update-service.test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# This simple script tests a few common cases for matching version pattern against artifact.
+# Note that it depends on a live OSS repository and may start failing at some point if release artifacts are deleted.
+
+. ./update-service.sh sourced
+
+readonly RELEASE_REPO=https://mvnrepo.cantara.no/content/repositories/releases
+readonly SNAPSHOT_REPO=https://mvnrepo.cantara.no/content/repositories/snapshots
+readonly GROUP_ID=net/whydah/token
+readonly ARTIFACT_ID=SecurityTokenService
+readonly DELAY=
+readonly USERNAME=
+readonly PASSWORD=
+
+echoerr() { echo "$@" 1>&2; }
+
+find_artifact_test() {
+    FAILED=false
+
+    find_artifact "2.4.*" jarfile url
+    if [[ "$jarfile" != "SecurityTokenService-2.4.19.jar" && "$url" != "https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19.jar" ]]; then
+        echoerr "EXPECTED:  SecurityTokenService-2.4.19.jar - https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19.jar"
+        echoerr "ACTUAL:    $jarfile - $url"
+        FAILED=true
+    fi
+
+    find_artifact "2.4.19" jarfile url
+    if [[ "$jarfile" != "SecurityTokenService-2.4.19.jar" && "$url" != "https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19.jar" ]]; then
+        echoerr "EXPECTED:  SecurityTokenService-2.4.19.jar - https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19.jar"
+        echoerr "ACTUAL:    $jarfile - $url"
+        FAILED=true
+    fi
+
+    find_artifact "SNAPSHOT" jarfile url
+    if [[ ! "$url" == *SNAPSHOT*.jar ]]; then
+        echoerr "EXPECTED:  to contain SNAPSHOT"
+        echoerr "ACTUAL:    $jarfile - $url"
+        FAILED=true
+    fi
+
+    if [[ "$FAILED" = true ]]; then
+        echoerr "Test failed!"
+        exit 1
+    fi
+}
+
+find_artifact_test
