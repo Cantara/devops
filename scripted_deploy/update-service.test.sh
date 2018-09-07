b7f994ea3a3024a1c89584febf45936578880b7d
Anders Rønning <andeero@ifi.uio.no>
2018-09-07 12:48:45 +0200

    Add support for classifier in update-service.sh

--- scripted_deploy/update-service.test.sh
+++ scripted_deploy/update-service.test.sh
@@ -32,7 +32,25 @@ find_artifact_test() {
     fi
 
     find_artifact "SNAPSHOT" jarfile url
+    echo "$URL"
     if [[ ! "$url" == *SNAPSHOT*.jar ]]; then
+        echoerr "EXPECTED:  to contain SNAPSHOT"
+        echoerr "ACTUAL:    $jarfile - $url"
+        FAILED=true
+    fi
+
+    # Find release with classifier
+    CLASSIFIER=jar-with-dependencies
+    find_artifact "2.4.19" jarfile url
+    if [[ "$jarfile" != "SecurityTokenService-2.4.19-jar-with-dependencies.jar" && "$url" != "https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19-jar-with-dependencies.jar" ]]; then
+        echoerr "EXPECTED:  SecurityTokenService-2.4.19-jar-with-dependencies.jar - https://mvnrepo.cantara.no/content/repositories/releases/net/whydah/token/SecurityTokenService/2.4.19/SecurityTokenService-2.4.19-jar-with-dependencies.jar"
+        echoerr "ACTUAL:    $jarfile - $url"
+        FAILED=true
+    fi
+
+    # Find snapshot with classifier
+    find_artifact "SNAPSHOT" jarfile url
+    if [[ ! "$url" == *SNAPSHOT*.jar ]] && [[ ! "$url" == *$CLASSIFIER ]]; then
         echoerr "EXPECTED:  to contain SNAPSHOT"
         echoerr "ACTUAL:    $jarfile - $url"
         FAILED=true
